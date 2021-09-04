b2fb7f00b7f3620a736d5c51bda731455704539a
Higor Luiz <hluiz@id.uff.br>
2021-09-04 18:38:32 -0300

    primeiro commit

---
+++ Assets/Plugins/Android/zzz.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Trunfo
+{
+    public class zzz : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
